1d44ed05d8049f63bc132d8666718036fc161b86
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-05 17:53:56 -0400

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,37 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  let [isOn, setIsOn] = useState(false);
+
+  function handleClick(){
+    setIsOn((isOn) => !isOn);
+  }
+
+  return (
+  <button style={{ background: "red" }} onClick={handleClick}>{isOn ? "ON" : "OFF"}
+  </button>
+  )
 }
 
 export default Toggle;
+
+// import React, { useState } from "react";
+
+// function Toggle() {
+//   const [isOn, setIsOn] = useState(false);
+
+//   function handleClick() {
+//     setIsOn((isOn) => !isOn);
+//   }
+
+//   const color = isOn ? "red" : "white";
+
+//   return (
+//     <button style={{ background: color }} onClick={handleClick}>
+//       {isOn ? "ON" : "OFF"}
+//     </button>
+//   );
+// }
+
+// export default Toggle;
